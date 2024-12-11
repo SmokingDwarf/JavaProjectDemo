ec5b57ca263e7493c801b1fd6741d0c11614f868
Michael <michael.vlaar@live.nl>
2024-12-11 16:59:43 +0100

    Practicum 3A

--- test/week1/practicum2B/VoetbalclubTest.java
+++ test/week1/practicum2B/VoetbalclubTest.java
@@ -11,19 +11,30 @@ class VoetbalclubTest
     private Voetbalclub test4;
 
     @BeforeEach
-    void VoetbalclubNEC() {
+    public void init() {
         this.test4 = new Voetbalclub("NEC");
     }
 
     @Test
-    void VoetbalclubConstructorTest() {
-
+    void VoetbalclubConstructorTestNull() {
         Voetbalclub test1 = new Voetbalclub(null);
         assertEquals("FC", test1.getNaam(), "Voetbalclub(null);");
+    }
 
+    @Test
+    void VoetbalclubConstructorTestLeegMetSpatie() {
+        Voetbalclub test2 = new Voetbalclub(" ");
+        assertEquals("FC", test2.getNaam(), "Voetbalclub(' ');");
+    }
+
+    @Test
+    void VoetbalclubConstructorTestLeegZonderSpatie() {
         Voetbalclub test2 = new Voetbalclub("");
         assertEquals("FC", test2.getNaam(), "Voetbalclub('');");
+    }
 
+    @Test
+    void VoetbalclubConstructorTestNEC() {
         Voetbalclub test3 = new Voetbalclub("NEC");
         assertEquals("NEC", test3.getNaam(), "Voetbalclub('NEC');");
     }
@@ -32,7 +43,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestWinstPunten() {
-        System.out.println("verwerkResultaatTestWinstPunten init");
         test4.verwerkResultaat('w');
         assertEquals(3, test4.aantalPunten(), "verwerkResultaatTestWinstPunten");
     }
@@ -40,21 +50,18 @@ class VoetbalclubTest
     @Test
     void verwerkResultaatTestWinstGespeeld()
     {
-        System.out.println("verwerkResultaatTestWinstGespeeld init");
         test4.verwerkResultaat('w');
         assertEquals(1, test4.aantalGespeeld(), "verwerkResultaatTestWinstGespeeld");
     }
 
     @Test
     void verwerkResultaatTestWinstToString() {
-        System.out.println("verwerkResultaatTestWinstToString init");
         test4.verwerkResultaat('w');
         assertEquals("NEC 1 1 0 0 3", test4.toString(), "verwerkResultaatTestWinstToString");
     }
 
     @Test
     void verwerkResultaatTestMeerdereWinstPunten(){
-        System.out.println("verwerkResultaatTestMeerdereWinst init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('w');
         assertEquals(6, test4.aantalPunten(), "verwerkResultaatTestMeerdereWinst");
@@ -62,7 +69,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereWinstGespeeld(){
-        System.out.println("verwerkResultaatTestMeerdereWinst init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('w');
         assertEquals(2, test4.aantalGespeeld(), "verwerkResultaatTestMeerdereWinst");
@@ -70,7 +76,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereWinstToString(){
-        System.out.println("verwerkResultaatTestMeerdereWinstToString init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('w');
         assertEquals("NEC 2 2 0 0 6", test4.toString(), "verwerkResultaatTestMeerdereWinstToString");
@@ -80,28 +85,24 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestGelijkspelPunten() {
-        System.out.println("verwerkResultaatTestGelijkspelPunten init");
         test4.verwerkResultaat('g');
         assertEquals(1, test4.aantalPunten(), "verwerkResultaatTestGelijkspelPunten");
     }
 
     @Test
     void verwerkResultaatTestGelijkspelGespeeld() {
-        System.out.println("verwerkResultaatTestGelijkspelGespeeld init");
         test4.verwerkResultaat('g');
         assertEquals(1, test4.aantalGespeeld(), "verwerkResultaatTestGelijkspelGespeeld");
     }
 
     @Test
     void verwerkResultaatTestGelijkspelToString() {
-        System.out.println("verwerkResultaatTestGelijkspelToString init");
         test4.verwerkResultaat('g');
         assertEquals("NEC 1 0 1 0 1", test4.toString(), "verwerkResultaatTestGelijkspelToString");
     }
 
     @Test
     void verwerkResultaatTestMeerdereGelijkspelPunten(){
-        System.out.println("verwerkResultaatTestMeerdereGelijkspelPunten init");
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('g');
         assertEquals(2, test4.aantalPunten(), "verwerkResultaatTestMeerdereGelijkspelPunten");
@@ -109,7 +110,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereGelijkspelGespeeld(){
-        System.out.println("verwerkResultaatTestMeerdereGelijkspelGespeeld init");
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('g');
         assertEquals(2, test4.aantalGespeeld(), "verwerkResultaatTestMeerdereGelijkspelGespeeld");
@@ -117,7 +117,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereGelijkspelToString(){
-        System.out.println("verwerkResultaatTestMeerdereGelijkspelToString init");
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('g');
         assertEquals("NEC 2 0 2 0 2", test4.toString(), "verwerkResultaatTestMeerdereGelijkspelToString");
@@ -128,7 +127,6 @@ class VoetbalclubTest
     @Test
     void verwerkResultaatTestVerliesPunten()
     {
-        System.out.println("verwerkResultaatTestVerliesPunten init");
         test4.verwerkResultaat('v');
         assertEquals(0, test4.aantalPunten(), "verwerkResultaatTestVerliesPunten");
     }
@@ -136,21 +134,18 @@ class VoetbalclubTest
     @Test
     void verwerkResultaatTestVerliesGespeeld()
     {
-        System.out.println("verwerkResultaatTestVerliesGespeeld init");
         test4.verwerkResultaat('v');
         assertEquals(1, test4.aantalGespeeld(), "verwerkResultaatTestVerliesGespeeld");
     }
 
     @Test
     void verwerkResultaatTestVerliesToString() {
-        System.out.println("verwerkResultaatTestVerliesToString init");
         test4.verwerkResultaat('v');
         assertEquals("NEC 1 0 0 1 0", test4.toString(), "verwerkResultaatTestVerliesToString");
     }
 
     @Test
     void verwerkResultaatTestMeerdereVerliesPunten(){
-        System.out.println("verwerkResultaatTestMeerdereVerliesPunten init");
         test4.verwerkResultaat('v');
         test4.verwerkResultaat('v');
         assertEquals(0, test4.aantalPunten(), "verwerkResultaatTestMeerdereVerliesPunten");
@@ -158,7 +153,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereVerliesGespeeld(){
-        System.out.println("verwerkResultaatTestMeerdereVerliesGespeeld init");
         test4.verwerkResultaat('v');
         test4.verwerkResultaat('v');
         assertEquals(2, test4.aantalGespeeld(), "verwerkResultaatTestMeerdereVerliesGespeeld");
@@ -166,7 +160,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereVerliesToString(){
-        System.out.println("verwerkResultaatTestMeerdereVerliesToString init");
         test4.verwerkResultaat('v');
         test4.verwerkResultaat('v');
         assertEquals("NEC 2 0 0 2 0", test4.toString(), "verwerkResultaatTestMeerdereVerliesToString");
@@ -177,28 +170,24 @@ class VoetbalclubTest
     @Test
     void verwerkResultaatTestOverigPunten()
     {
-        System.out.println("verwerkResultaatTestOverigPunten init");
         test4.verwerkResultaat('c');
         assertEquals(0, test4.aantalPunten(), "verwerkResultaatTestOverigPunten");
     }
 
     @Test
     void verwerkResultaatTestOverigGespeeld() {
-        System.out.println("verwerkResultaatTestOverigGespeeld init");
         test4.verwerkResultaat('c');
         assertEquals(0, test4.aantalGespeeld(), "verwerkResultaatTestOverigGespeeld");
     }
 
     @Test
     void verwerkResultaatTestOverigToString() {
-        System.out.println("verwerkResultaatTestOverigToString init");
         assertEquals("NEC 0 0 0 0 0", test4.toString(), "verwerkResultaatTestOverigToString");
     }
 
     // Divers
     @Test
     void verwerkResultaatTestMeerdereDiversePunten(){
-        System.out.println("verwerkResultaatTestMeerdereDiversePunten init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('v');
@@ -207,7 +196,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereDiverseGespeeld(){
-        System.out.println("verwerkResultaatTestMeerdereDiverseGespeeld init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('v');
@@ -216,7 +204,6 @@ class VoetbalclubTest
 
     @Test
     void verwerkResultaatTestMeerdereDiverseToString(){
-        System.out.println("verwerkResultaatTestMeerdereDiverseToString init");
         test4.verwerkResultaat('w');
         test4.verwerkResultaat('g');
         test4.verwerkResultaat('v');
